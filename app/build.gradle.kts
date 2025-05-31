ad31079679d1d7c072221d7dfe1d66c884e8bb27
veronikavanivska <veronikavanivska2005@gmail.com>
2025-05-31 14:25:49 +0200

    All required files also the fonts,drawable,values

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.splitbooks"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.splitbooks"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        vectorDrawables.useSupportLibrary = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.annotation)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+}
---
+++ app/src/main/java/com/example/splitbooks/LoginActivity.java
@@ -0,0 +1,123 @@
+package com.example.splitbooks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.splitbooks.DTO.LoginRequest;
+import com.example.splitbooks.DTO.LoginResponse;
+import com.example.splitbooks.network.ApiClient;
+import com.example.splitbooks.network.ApiService;
+import com.example.splitbooks.network.JwtManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private ImageView eyeIcon;
+    private EditText emailField, passwordField;
+    private Button loginButton;
+    private TextView registerText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailField = findViewById(R.id.email_field);
+        passwordField = findViewById(R.id.password_field);
+        loginButton = findViewById(R.id.login_button);
+        registerText = findViewById(R.id.register_text);
+        eyeIcon = findViewById(R.id.eye_icon);
+
+        passwordField.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+
+        eyeIcon.setOnClickListener(v -> {
+
+            int cursorPosition = passwordField.getSelectionStart();
+
+            if (passwordField.getTransformationMethod() == PasswordTransformationMethod.getInstance()) {
+
+                passwordField.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                eyeIcon.setImageResource(R.drawable.eye);
+            } else {
+
+                passwordField.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                eyeIcon.setImageResource(R.drawable.eye_closed);
+            }
+            passwordField.setSelection(cursorPosition);
+        });
+
+        registerText.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        loginButton.setOnClickListener(v -> {
+            String email = emailField.getText().toString().trim();
+            String password = passwordField.getText().toString().trim();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "Please fill in both fields", Toast.LENGTH_SHORT).show();
+            } else {
+                loginUser(email, password);
+            }
+        });
+    }
+
+    public void loginUser(String email, String password) {
+        try {
+            ApiService apiService = ApiClient.getApiService(getApplicationContext());
+            LoginRequest loginRequest = new LoginRequest(email, password);
+
+            Call<LoginResponse> call = apiService.login(loginRequest);
+            call.enqueue(new Callback<LoginResponse>() {
+                @Override
+                public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                    if (response.isSuccessful()) {
+
+                        LoginResponse loginResponse = response.body();
+                        if (loginResponse != null) {
+
+                            JwtManager.saveToken(getApplicationContext(), loginResponse.getToken());
+                            Log.d("LoginActivity", "Saved Token: " + loginResponse.getToken());
+
+//                            String userToken = loginResponse.getToken();
+//                            long userId = loginResponse.getUserId();
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Login failed. Please check your credentials.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<LoginResponse> call, Throwable t) {
+                    Toast.makeText(LoginActivity.this, "Network error. Please try again.", Toast.LENGTH_SHORT).show();
+                    t.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(LoginActivity.this, "An unexpected error occurred.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
